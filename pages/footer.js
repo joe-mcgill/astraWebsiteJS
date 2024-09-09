d3b14d914ecba02d22ce43c427a533db4abcb595
joe-mcgill <joe@josephmcgill.com>
2024-09-09 17:12:16 -0400

    fixing icons headers and footers

--- pages/footer.js
+++ pages/footer.js
@@ -1,41 +1,18 @@
 document.write('\
 \
-<footer class="bg-light text-center ">\
-  <div class="container p-4">\
+<footer class=" row bg-light text-center ">\
+  <div class="col-md-4">\
   \
-    <section class="mb-4">\
       <a class="btn btn-primary btn-floating m-1" style="background-color: #3b5998" href="#!" role="button"><i class="fab fa-facebook-f"></i></a>\
 \
       <a class="btn btn-primary btn-floating m-1" style="background-color: #dd4b39" href="#!" role="button"><i class="fab fa-google"></i></a>\
 \
-      <a class="btn btn-primary btn-floating m-1" style="background-color: #ac2bac" href="#!" role="button"><i class="fab fa-instagram"></i></a>\
-    </section>\
-    <section class="">\
-      <form action="">\
-        \
-        <div class="row d-flex justify-content-center">\
-          <div class="col-auto">\
-            <p class="pt-2">\
-              <strong>Sign up for our newsletter</strong>\
-            </p>\
-          </div>\
-          <div class="col-md-5 col-12">\
-            <div class="form-outline mb-4">\
-              <input type="email" id="form5Example2" class="form-control" />\
-              <label class="form-label" for="form5Example2">Email address</label>\
-            </div>\
-          </div>\
-          <div class="col-auto">\
-            <button type="submit" class="btn btn-primary mb-4">\
-              Subscribe\
-            </button>\
-          </div>\
-        </div>\
-      </form>\
-    </section>\
+      <a class="btn btn-primary btn-floating m-1" style="background-color: #fff" href="#!" role="button"><i class="bi bi-instagram"></i></a>\
   </div>\
-  <div class="text-right p-3" style="background-color: rgba(0, 0, 0, 0.2)">\
-    photos by <href="https://http://www.anaelenaphotography.com/" target="_blank" >@anaelenaphotography</a>\
+  <div class="col-md-8 text-links">\
+  <span>For more info:</span>\
+  <a href="mailto:astra@astrarooney.com" ><i class="bi bi-envelope-heart"></i>astra@astrarooney.com</a>\
   </div>\
+  \
 </footer>\
 ');
---
+++ pages/navbar.js
@@ -0,0 +1,42 @@
+document.write('\
+    <nav class="navbar navbar-expand-lg justify-content-between fixed-top">\
+<header class="container-fluid">\
+      <a class="navbar-brand" href="#" style="display:inline-block">\
+        <img src="images/cropped-image218.png" height="100px" >\
+    </a>\
+    <div class="pagetitle collapse navbar-light navbar-collapse">Astra Rooney Massage</div>\
+  <button class="navbar-toggler" type="button" data-toggle="collapse"  data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">\
+    <svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" fill="blue" class="bi bi-arrow-bar-down" viewBox="0 0 16 16">\
+        <path fill-rule="evenodd" d="M1 3.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5M8 6a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 .708-.708L7.5 12.293V6.5A.5.5 0 0 1 8 6"/>\
+    </svg>\
+  </button>\
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">\
+    <ul class="navbar-nav mr-auto navbar-menu-ul text-links">\
+      <li class="nav-item active">\
+        <a class="nav-link" href="#">About Me <span class="sr-only">(current)</span></a>\
+      </li>\
+      <li class="nav-item">\
+        <a class="nav-link" href="#">Services Offered</a>\
+      </li>\
+      <li class="nav-item">\
+        <a class="nav-link" href="#">Location</a>\
+      </li>\
+      <li class="nav-item">\
+        <a class="nav-link" href="#">Reviews</a>\
+      </li>\
+      <li class="nav-item">\
+        <a class="nav-link" href="#">Book Now</a>\
+      </li>\
+      <li class="nav-item">\
+        <a class="nav-link" href="#">Resources</a>\
+      </li>\
+      <li class="nav-item">\
+        <a class="nav-link" href="#">Contact</a>\
+      </li>\
+      \
+    </ul>\
+    \
+  </div>\
+</nav>\
+</header>\
+');
