b818f328704f7eebcb79b594976d1aeb78b01a9f
joe-mcgill <joe@josephmcgill.com>
2024-09-09 21:23:37 -0400

    stopping for today

--- pages/navbar.js
+++ pages/navbar.js
@@ -4,7 +4,7 @@ document.write('\
       <a class="navbar-brand" href="#" style="display:inline-block">\
         <img src="images/cropped-image218.png" height="100px" >\
     </a>\
-    <div class="pagetitle collapse navbar-light navbar-collapse">Astra Rooney Massage</div>\
+    <div class="pagetitle d-block d-md-none">Astra Rooney Massage</div>\
   <button class="navbar-toggler" type="button" data-toggle="collapse"  data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">\
     <svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" fill="blue" class="bi bi-arrow-bar-down" viewBox="0 0 16 16">\
         <path fill-rule="evenodd" d="M1 3.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5M8 6a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 .708-.708L7.5 12.293V6.5A.5.5 0 0 1 8 6"/>\
