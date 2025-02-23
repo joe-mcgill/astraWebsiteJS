cb2996beafca978f96d2728cd33fc9efd306d0b1
Joe Mcgill <joe@josephmcgill.com>
2025-02-22 23:52:49 -0500

    v1 draft

--- pages/navbar.js
+++ pages/navbar.js
@@ -3,21 +3,19 @@ document.write('\
 \
       <a class="navbar-brand mr-auto col-auto d-lg-print-inline-flex" href="index.html">\
       <div style="display:flex; align-items:center;">\
-        <img src="images/cropped-image218.png" height="100px" >\
-        <h5 class="pagetitle d-none d-xl-block">Astra Rooney Massage</h5>\
+        <img src="images/cropped-image218.png" height="75px" >\
+        <h6 class="pagetitle d-none d-xl-block">Astra Rooney Massage</h5>\
       </div>\
     </a>\
     <button class="navbar-toggler" type="button" data-toggle="collapse"  data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">\
-    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="blue" class="menu-hamburger" viewBox="0 0 16 16">\
-<path fill="#EFD9C1" d="M1,4 H18 V6 H1 V4 M1,9 H18 V11 H1 V7 M3,14 H18 V16 H1 V14" />/        </svg>\
-  </button>\
+<svg width="50px" height="50px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bluegCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 18L20 18" stroke="#2035a8" stroke-width="2" stroke-linecap="round"></path> <path d="M4 12L20 12" stroke="#2035a8" stroke-width="2" stroke-linecap="round"></path> <path d="M4 6L20 6" stroke="#2035a8" stroke-width="2" stroke-linecap="round"></path> </g></svg>  </button>\
   <div class="collapse navbar-collapse" id="navbarSupportedContent">\
     <ul class="navbar-nav mr-auto navbar-menu-ul text-links">\
       <li class="nav-item active">\
         <a class="nav-link" href="aboutme.html">About Me <span class="sr-only">(current)</span></a>\
       </li>\
       <li class="nav-item">\
-        <a class="nav-link" href="#">Services Offered</a>\
+        <a class="nav-link" href="services.html">Services Offered</a>\
       </li>\
       <li class="nav-item">\
         <a class="nav-link" href="location.html">Location</a>\
@@ -26,13 +24,10 @@ document.write('\
         <a class="nav-link" href="reviews.html">Reviews</a>\
       </li>\
       <li class="nav-item">\
-        <a class="nav-link" href="#">Book Now</a>\
+        <a class="nav-link" href="book.html">Book Now</a>\
       </li>\
       <li class="nav-item">\
-        <a class="nav-link" href="#">Resources</a>\
-      </li>\
-      <li class="nav-item">\
-        <a class="nav-link" href="#">Contact</a>\
+        <a class="nav-link" href="resources.html">Resources</a>\
       </li>\
       \
     </ul>\
