3c2172fef07b061d35a4f8712e2fb702da74c4e1
Joe Mcgill <joe@josephmcgill.com>
2025-02-23 13:19:35 -0500

    refactored three buttons code

---
+++ pages/threebuttons.js
@@ -0,0 +1,17 @@
+document.write('\
+ <div class="flex-container">\
+                <div style="flex-shrink: 0;">  \
+                <a href="https://www.massagebook.com/biz/AstraRooney#services" target="_blank">\
+                    <img width="200px" style="max-width: 95%;" src="http://www.massagebook.com/home/img/getbutton/button-booknow.png" alt="Book Now on MassageBook.com!"/>\
+                </a>\
+                </div>\
+                <div style="flex-shrink: 0;">\
+                <a href="https://www.massagebook.com/therapists/AstraRooney/gift-certificates?src=external" target="_blank" rel="noopener">\
+                    <img width="150px" style="max-width: 95%; min-width: 95%"  src="http://www.massagebook.com/home/img/button-gift.png" alt="Purchase gift card on MassageBook.com!">\
+                </a>\
+                </div>\
+                <div >\
+                <iframe  src="https://www.massagebook.com/therapists/AstraRooney/widget/reviews-btn"  frameborder="0" width="185" height="130" margin-left="-20px" allowtransparency="false" style="padding:2px;background:#2035a8;webkit-border-radius: 5px;-moz-border-radius: 5px;border-radius: 5px;overflow:hidden;"></iframe>\
+                </div>\
+            </div>\
+');
