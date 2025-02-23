ff3ea2d1fa74d78a022535a455e79a2bd7fff070
Joe Mcgill <joe@josephmcgill.com>
2025-02-22 20:26:58 -0500

    minimal footer

--- pages/footer.js
+++ pages/footer.js
@@ -1,15 +1,7 @@
 document.write('\
 \
 <footer class=" row bg-light text-center ">\
-  <div class="col-md-4">\
-  \
-      <a class="btn btn-primary btn-floating m-1" style="background-color: #3b5998" href="#!" role="button"><i class="fab fa-facebook-f"></i></a>\
-\
-      <a class="btn btn-primary btn-floating m-1" style="background-color: #dd4b39" href="#!" role="button"><i class="fab fa-google"></i></a>\
-\
-      <a class="btn btn-primary btn-floating m-1" style="background-color: #fff" href="#!" role="button"><i class="bi bi-instagram"></i></a>\
-  </div>\
-  <div class="col-md-8 text-links">\
+  <div class="col-md-12 text-links">\
   <span>For more info:</span>\
   <a href="mailto:astra@astrarooney.com" ><i class="bi bi-envelope-heart"></i>astra@astrarooney.com</a>\
   </div>\
