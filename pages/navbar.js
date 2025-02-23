42eb4271ab64248b4812cbaceaf252034493214c
Joe Mcgill <joe@josephmcgill.com>
2025-02-22 20:44:07 -0500

    switched to hamburger menu icon

--- pages/navbar.js
+++ pages/navbar.js
@@ -4,13 +4,12 @@ document.write('\
       <a class="navbar-brand mr-auto col-auto d-lg-print-inline-flex" href="#">\
       <div style="display:flex; align-items:center;">\
         <img src="images/cropped-image218.png" height="100px" >\
-        <h4 class="pagetitle d-none d-xl-block">Astra Rooney Massage</h4>\
+        <h5 class="pagetitle d-none d-xl-block">Astra Rooney Massage</h5>\
       </div>\
     </a>\
     <button class="navbar-toggler" type="button" data-toggle="collapse"  data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">\
-    <svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" fill="blue" class="bi bi-arrow-bar-down" viewBox="0 0 16 16">\
-        <path fill-rule="evenodd" d="M1 3.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5M8 6a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 .708-.708L7.5 12.293V6.5A.5.5 0 0 1 8 6"/>\
-    </svg>\
+    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="blue" class="menu-hamburger" viewBox="0 0 16 16">\
+<path fill="#EFD9C1" d="M1,4 H18 V6 H1 V4 M1,9 H18 V11 H1 V7 M3,14 H18 V16 H1 V14" />/        </svg>\
   </button>\
   <div class="collapse navbar-collapse" id="navbarSupportedContent">\
     <ul class="navbar-nav mr-auto navbar-menu-ul text-links">\
