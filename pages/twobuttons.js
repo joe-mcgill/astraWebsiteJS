d70b720dc479416eea56e9e10ff6666637fc8c63
Joe Mcgill <joe@josephmcgill.com>
2025-03-08 20:40:02 -0500

    reviews page finally centered

---
+++ pages/twobuttons.js
@@ -0,0 +1,14 @@
+document.write('\
+ <div class="flex-container">\
+                <div style="flex-shrink: 0; justify-content: flex-end;">  \
+                <a href="https://www.massagebook.com/biz/AstraRooney#services" target="_blank">\
+                    <img width="200px" style="max-width: 95%;" src="http://www.massagebook.com/home/img/getbutton/button-booknow.png" alt="Book Now on MassageBook.com!"/>\
+                </a>\
+                </div>\
+                <div style="flex-shrink: 0;">\
+                <a href="https://www.massagebook.com/therapists/AstraRooney/gift-certificates?src=external" target="_blank" rel="noopener">\
+                    <img width="150px" style="max-width: 95%; min-width: 95%"  src="http://www.massagebook.com/home/img/button-gift.png" alt="Purchase gift card on MassageBook.com!">\
+                </a>\
+                </div>\
+                </div>\
+');
